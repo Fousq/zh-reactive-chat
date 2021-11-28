cfa67ea29e78e4d22cbc235f42937154f54c45cd
Zhanbolat Orakbayev <fous363@gmail.com>
2021-11-28 15:01:30 +0600

    feature: Implement receive message method

---
+++ app/src/main/java/kz/zhanbolat/chat/service/ChatServiceBean.java
@@ -0,0 +1,17 @@
+package kz.zhanbolat.chat.service;
+
+import com.google.common.base.Strings;
+import kz.zhanbolat.chat.entity.ChatMessage;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+public class ChatServiceBean implements ChatService {
+
+
+    @Override
+    public ChatMessage receiveMessage(ChatMessage message) {
+
+        return null;
+    }
+}
